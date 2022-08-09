87e1f3da0000e47d6061b3707167addc1156415f
Rodrigo Gonçalves <rodrigo.goncalves1101@gmail.com>
2022-08-09 19:59:20 +0100

    Initial commit

---
+++ src/go-movies-crud/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for i, item := range movies {
+
+		if item.ID == params["id"] {
+			movies = append(movies[:i], movies[i+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			break
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+
+	movies = append(movies, movie)
+
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	
+	params := mux.Vars(r)
+
+	for i, item := range movies {
+
+		if item.ID == params["id"] {
+			_ = json.NewDecoder(r.Body).Decode(&movies[i])
+			json.NewEncoder(w).Encode(movies[i])
+			break
+		}
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	jhon := Director{Firstname: "Jhon", Lastname: "Sin"}
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &jhon})
+	movies = append(movies, Movie{ID: "2", Isbn: "732389", Title: "Movie Two", Director: &jhon})
+
+	router.HandleFunc("/movies", getMovies).Methods("GET")
+	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	router.HandleFunc("/movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
